c40b4a6ae9e0ed523f6d50504a8a2398513155ba
Manh-coder <manhnthe170866@fpt.edu.vn>
2025-03-21 10:17:05 +0700

    fix code about manager quantities

--- SWP_G4/src/java/controller/UpdateProductLineServlet.java
+++ SWP_G4/src/java/controller/UpdateProductLineServlet.java
@@ -88,6 +88,8 @@ public class UpdateProductLineServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    
+    //update
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
